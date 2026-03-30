804922a4bba642a6697a30a9d7d2812757b2d9ed
SquigglySquare <justasillyguymaybe@gmail.com>
2026-03-30 17:52:13 -0400

    GRAVITY

--- stolen-idea/movement.gd
+++ stolen-idea/movement.gd
@@ -6,10 +6,11 @@ extends RigidBody3D
 
 
 func _physics_process(_delta: float) -> void:
-
+	
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = ($camAnchor.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
 		print(direction)
 		apply_impulse(direction * ACC * Vector3(1,0,1).normalized())
+	
